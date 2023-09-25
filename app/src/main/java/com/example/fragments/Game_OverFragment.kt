a74ec04c24f227f1b4866db3cb83a47e7e8d7eaf
RitaPintoBissApp <rpinto@blissapplications.com>
2023-09-25 10:24:50 +0100

    added files

---
+++ app/src/main/java/com/example/fragments/Game_OverFragment.kt
@@ -0,0 +1,22 @@
+package com.example.fragments
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.fragments.databinding.ActivityMainBinding
+import com.example.fragments.databinding.FragmentGameOverBinding
+
+class GameOverFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game__over, container, false)
+        return binding.root
+    }
+}
