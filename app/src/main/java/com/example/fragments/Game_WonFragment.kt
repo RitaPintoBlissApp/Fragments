13e573e20728c52f19bb22ed11cc723c81af0687
RitaPintoBissApp <rpinto@blissapplications.com>
2023-09-25 18:38:29 +0100

    added files

--- app/src/main/java/com/example/fragments/Game_WonFragment.kt
+++ app/src/main/java/com/example/fragments/Game_WonFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.GameFragment
 import com.example.fragments.databinding.FragmentGameWonBinding
 
@@ -16,6 +17,10 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_game__won, container, false)
+        // Add OnClick Handler for Next Match button
+        binding.nextMatchButton.setOnClickListener{view: View->
+            view.findNavController()
+                .navigate(R.id.action_gameWonFragment_to_gameFragment)}
         return binding.root
     }
 }
